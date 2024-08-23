c459eb0bd31537bca229335a8504f11c7145a9c1
sohal rahaman <sohalr1305@gmail.com>
2024-08-23 22:48:34 +0530

    new backend updates

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -1,47 +1,53 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { createError } from './error.js';
 
-export const verifyToken = (req,res,next)=>{
-    const token = req.cookies.plantebuy_token;
-    console.log(token);
-    if(!token){
-        return next(createError(404,"User Is not Authneticated"))
+// Function to verify token and attach user to request object
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.plantebuy_token || req.headers['authorization']?.split(' ')[1];
+    
+    if (!token) {
+        return next(createError(401, "Authentication token is missing or invalid"));
     }
-    jwt.verify(token,process.env.JWT_SECRET_KEY,(err,user)=>{
-        if(err) next(createError(404,"Token is Not Valid"));
+    
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return next(createError(403, "Token is not valid"));
+        }
+        
         req.user = user;
         next();
-    })
-}
-
-export const verifyUser = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-    if(req.user.id){
-        next();
-    }else{
-    return next(createError(404,"You not Authneticated"))
-    }
-})
+    });
 };
 
-export const verifySeller = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-    if(req.user.seller){
-        next();
-    }else{
-        console.log(req.user);
-    return next(createError(404,"You not Authneticated (only Admin are Allowes)"))
-    }
-})
-}
-
-export const verifyAdmin = (req,res,next)=>{
-        verifyToken(req,res,()=>{
-        if(req.user.admin){
+// Middleware to verify if the user is authenticated
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user?.id) {
             next();
-        }else{
-            console.log(req.user);
-        return next(createError(404,"You not Authneticated (only Admin are Allowes)"))
+        } else {
+            return next(createError(401, "Unauthorized access"));
         }
-    })
-}
+    });
+};
+
+// Middleware to verify if the user is a seller
+export const verifySeller = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user?.seller) {
+            next();
+        } else {
+            return next(createError(403, "Forbidden: Only sellers are allowed"));
+        }
+    });
+};
+
+// Middleware to verify if the user is an admin
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user?.admin) {
+            next();
+        } else {
+            return next(createError(403, "Forbidden: Only admins are allowed"));
+        }
+    });
+};
