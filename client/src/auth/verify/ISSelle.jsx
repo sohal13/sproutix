d544027c5bd178cc8c3bcb222679a7f00c0c5dc8
sohal rahaman <sohalr1305@gmail.com>
2024-08-25 13:54:00 +0530

    frontend auth

--- client/src/auth/verify/ISSelle.jsx
+++ client/src/auth/verify/ISSelle.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { userAuth } from '../../contextAPI/authContext'
 import { Navigate, Outlet } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 const ISSeller = () => {
     const {authUser} = userAuth();
--- client/src/auth/verify/ISUser.jsx
+++ client/src/auth/verify/ISUser.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { userAuth } from '../../contextAPI/authContext'
 import { Navigate, Outlet } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 const ISUser = () => {
     const {authUser} = userAuth();
     console.log(authUser);
    
   return (
-    authUser ? <Outlet/> : <Navigate to={'/'} notfication={toast.info('Sorry!! Only User Can Access the Page',{autoClose:2000})}/>
+    authUser ? <Outlet/> : <Navigate to={'/login'}/>
   )
 }
 
